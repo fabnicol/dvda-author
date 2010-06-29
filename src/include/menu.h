e53ea20bf69372b020c7da40985f241a308864d1
Fabrice Nicol <fabnicol@users.sourceforge.net>
2010-06-30 00:08:54 +0200

    branch2

--- src/include/menu.h
+++ src/include/menu.h
@@ -11,4 +11,6 @@ int mogrify_img(char* text, int8_t group, int8_t track, pic *img, uint8_t maxnum
 int  generate_menu_pics(pic* img, uint8_t ngroups, uint8_t *numtracks, uint8_t maxntracks);
 int create_stillpic_directory(char* string, uint32_t count);
 void create_activemenu(pic* img,uint16_t totntracks);
+uint16_t x(uint8_t group, uint8_t ngroups);
+uint16_t y(uint8_t track, uint8_t maxnumtracks);
 #endif // HAVE_MENU_C
