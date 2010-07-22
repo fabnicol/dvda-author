2373e1bd91abfaf5c8a3ea36203c6fb02d484abf
U-DF5P3L2J\Ordinateur Personnel <Ordinateur Personnel@DPE_RESEAU.(none)>
2010-07-22 11:24:55 +0200

    premerge2
    Former-commit-id: 14da7e00e0020f23143a177dc7133a257d47605e

---
+++ src/include/version.h
@@ -0,0 +1,12 @@
+#ifndef VERSION_H_INCLUDED
+#define VERSION_H_INCLUDED
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef VERSION
+#define VERSION "10.06-300"
+#endif
+
+#endif // VERSION_H_INCLUDED
