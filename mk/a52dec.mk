44cb1b5264df39effe3657d812783c5d6e0b14cb
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2013-10-08 03:06:56 +0200

    progress toward généralisation of MXE-style support
    Former-commit-id: ac8fe9a440389b18d5243f23e142693cd31454fe [formerly d46f785da240605ef32e30018996a9c5578f3f0d]
    Former-commit-id: 8ee349bae7f0bc53504e36db09e2564ee2f2c1d5

--- mk/a52dec.mk
+++ mk/a52dec.mk
@@ -1,3 +1,6 @@
-a52dec: Makefile
-	$(call config_exec_package,$@,$@,2>&1|line)
+a52dec_MAKESPEC=auto
+a52dec_CONFIGSPEC=exe
+a52dec_DEPENDENCY=Makefile
+a52dec_TESTBINARY=a52dec
+a52dec_COMMANDLINE=2>&1|line
 
