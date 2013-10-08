ac8fe9a440389b18d5243f23e142693cd31454fe
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2013-10-08 03:06:56 +0200

    progress toward généralisation of MXE-style support
    Former-commit-id: d46f785da240605ef32e30018996a9c5578f3f0d

--- mk/libFlac.mk
+++ mk/libFlac.mk
@@ -1,7 +1,5 @@
-
-FLAC: Makefile libogg
-	$(call config_lib_package,libFLAC)
-
-
-# you need the autotools to be able to rebuild sox with the sox-libs patch (the alternative would be using pkg-config, which is no better)
+FLAC_MAKESPEC=auto
+FLAC_CONFIGSPEC=lib
+FLAC_DEPENDENCY=Makefile libogg
+FLAC_TARGETLIB=libFLAC
 
