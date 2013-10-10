b10c9b910af9e9ffcacd2f6aa8dbf8edaf89c4b6
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2013-10-10 23:56:36 +0200

    recreate src/Makefile.am
    Former-commit-id: 567e44cc309b6d69237ab2e730259a900bf82781

--- mk/mpeg2dec.mk
+++ mk/mpeg2dec.mk
@@ -1,6 +1,13 @@
-libmpeg2dec_MAKESPEC=auto
-libmpeg2dec_CONFIGSPEC=exe
-libmpeg2dec_DEPENDENCY=Makefile
-libmpeg2dec_TESTBINARY=mpeg2dec
-libmpeg2dec_COMMANDLINE=<null 2>&1|line
+libmpeg2_MAKESPEC=auto
+libmpeg2_CONFIGSPEC=exe
+libmpeg2_DEPENDENCY=Makefile
+libmpeg2_TESTBINARY=mpeg2dec
+libmpeg2_COMMANDLINE=<null 2>&1|line
 
+
+#Autoconf-substituted, do not modify#
+libmpeg2_LIB=@libmpeg2_LIB@
+libmpeg2_LINK=@libmpeg2_LINK@
+MAYBE_libmpeg2=@MAYBE_libmpeg2@
+HAVE_libmpeg2dec=@HAVE_libmpeg2dec@
+HAVE_EXTERNAL_mpeg2dec=@HAVE_EXTERNAL_mpeg2dec@
