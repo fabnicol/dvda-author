5a3262e1ac7475d9fa342b88ea164f6df11d270e
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2013-10-23 02:03:39 +0200

    apply rpl_malloc trick
    Former-commit-id: f7f71e0259d7d3e5b9df999da0e4e69402fc81d0

--- libutils/src/libc_utils.c
+++ libutils/src/libc_utils.c
@@ -81,6 +81,20 @@ void pause_dos_type()
 
 
 }
+#undef malloc
+   
+
+void *malloc ();
+
+/* Allocate an N-byte block of memory from the heap.
+   If N is zero, allocate a 1-byte block.  */
+   
+void* rpl_malloc (size_t n)
+{
+ if (n == 0)
+    n = 1;
+ return malloc (n);
+}
 
 
 void erase_file(const char* path)
