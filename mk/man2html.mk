88971dc9ddb3fe66ff9952f12623f9002eaa8fa0
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2016-08-10 02:09:14 +0200

    identify Sox convert bug: infinite loop on conversion (eg mp3 -> wav)é
    Former-commit-id: 87abb887fbf4139cde44559cb63aaf2a96392ae7

--- mk/man2html.mk
+++ mk/man2html.mk
@@ -1,10 +1,10 @@
 
-include /mnt/fab/Dev/dvda-author-dev/BUILD/mk/man2html.global.mk
+include /home/fab/Dev/dvda-author-dev/BUILD/mk/man2html.global.mk
 
 man2html_MAKESPEC=auto
 man2html_CONFIGSPEC=exe
 man2html_TESTBINARY=man2html
 
-/mnt/fab/Dev/dvda-author-dev/BUILD/depconf/man2html.depconf: $(man2html_DEPENDENCY)
+/home/fab/Dev/dvda-author-dev/BUILD/depconf/man2html.depconf: $(man2html_DEPENDENCY)
 	$(call depconf,man2html,noconfigure,$(CONFIGURE_man2html_FLAGS) CFLAGS=)
 
