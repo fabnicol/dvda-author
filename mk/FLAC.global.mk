1409f7b3ed95785f4e1a1de168cf079a8c5096bf
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2016-08-12 21:57:35 +0200

    add packages
    Former-commit-id: 18cd0efbbb1466a14e443dc02266eef63ef303f4 [formerly 57e0c7a5d10db2dd4fade4a409ede76cb8f513da]
    Former-commit-id: e8c87b7bb54f5f5673f2da01888c78e72b5d972a

--- mk/FLAC.global.mk
+++ mk/FLAC.global.mk
@@ -1,9 +1,9 @@
-FLAC_LIB=/home/fab/Dev/dvda-author-dev/local/lib/libFLAC.a
+FLAC_LIB=
 FLAC_LINK=
-MAYBE_FLAC=flac-1.3.1
+MAYBE_FLAC=
 HAVE_FLAC=@HAVE_FLAC@
 HAVE_EXTERNAL_FLAC=@HAVE_EXTERNAL_FLAC@
-CONFIGURE_FLAC_FLAGS=--enable-static --disable-shared --disable-fast-install --with-ogg-libraries=/home/fab/Dev/dvda-author-dev/local/lib --with-ogg-includes=/home/fab/Dev/dvda-author-dev/local/include/ogg        --disable-thorough-tests --disable-oggtest --disable-doxygen-docs --disable-xmms-plugin --disable-doxygen-docs --prefix=/home/fab/Dev/dvda-author-dev/local CPPFLAGS=-I/home/fab/Dev/dvda-author-dev/local/include 
+CONFIGURE_FLAC_FLAGS=
 flac_BUILD=@flac_BUILD@
 WITH_FLAC=@WITH_FLAC@
-FLAC_DEPENDENCY=Makefile /home/fab/Dev/dvda-author-dev/flac-1.3.1
+FLAC_DEPENDENCY=Makefile c:/Users/Public/Dev/dvda-author-dev/
