40a3fd7561a79396a572ab17c78cc1ac5945004e
Fabrice Nicol <fabrnicol@users.sourceforge.net>
2016-08-26 16:53:21 +0200

    implement WAV_FORMAT_EXTENSIBLE (cont)
    Former-commit-id: 8c29cb485c994ca936e2975821a2f314131064bf
    Former-commit-id: 46895b2dc7d99d11f20c68f3e5b6a2e8183a6cc3

--- libfixwav/src/readHeader.c
+++ libfixwav/src/readHeader.c
@@ -34,11 +34,11 @@ int readHeader(FILE * infile, WaveHeader *header)
 
 
   uint8_t *p;
-  uint8_t temp[header->header_size_in];
-  memset(temp, 0, header->header_size_in);
-  p=temp;
+  uint8_t buffer[header->header_size_in];
+  memset(buffer, 0, header->header_size_in);
+  p=buffer;
   rewind(infile);
-  count=fread(temp, 1, header->header_size_in, infile ) ;
+  count=fread(buffer, 1, header->header_size_in, infile ) ;
   /* Total is 44 bytes */
 
   if  (count != header->header_size_in)
@@ -51,11 +51,11 @@ int readHeader(FILE * infile, WaveHeader *header)
 
 #if 0
 
-  Remember the structure of a header is:
+  Remember the structure of a WAVE_FORMAT_PCM header is:
   (standard header)
   uint8_t header[44]=
-  {'R','I','F','F',    //  0 - ChunkID
-    0,0,0,0,            //  4 - ChunkSize (filesize-8)
+  {'R','I','F','F',    //   0 - ChunkID
+    0,0,0,0,            //  4 - ChunkSize (filesize - 8 - padbyte)
     'W','A','V','E',    //  8 - Format
     'f','m','t',' ',    // 12 - SubChunkID
     16,0,0,0,           // 16 - SubChunk1ID  // 16 for PCM
@@ -66,30 +66,42 @@ int readHeader(FILE * infile, WaveHeader *header)
     4,0,                // 32 - BlockAlign (== NumChannels * BitsPerSample/8)
     16,0,               // 34 - BitsPerSample
     'd','a','t','a',    // 36 - Subchunk2ID
-    0,0,0,0             // 40 - 43 Subchunk2Size
+    0,0,0,0             // 40 - 43 Subchunk2Size = Chunksize - 36 = filesize - 44 - padbyte
   };
-  
+
+  Samples follow.
+  Pad byte at EF if sample count is odd.
+
   or else (extended wav)
-  uint8_t header[57]=
+  N is the offset of "data" + 8.
+  N = 46 (minimal extension, no fact), 58 (minimal extension, fact), 68 (long extension, no fact) or 80 (long extention, minimal fact) or 80 + x (long extension, non-standard tagged fact)
+
+  uint8_t header[N]=
   {'R','I','F','F',    //  0 - ChunkID
-    0,0,0,0,            //  4 - ChunkSize (filesize-8)
+    0,0,0,0,            //  4 - ChunkSize (filesize - 8 - padbyte)
     'W','A','V','E',    //  8 - Format
     'f','m','t',' ',    // 12 - SubChunkID
-    16,0,0,0,           // 16 - SubChunk1ID  // 16 for PCM
+    40,0,0,0,           // 16 - SubChunk1ID  // 18 or 40 for PCM as 16 is only for WAVE_FORMAT_PCM
     1,0,                // 20 - AudioFormat (1=16-bit)
     2,0,                // 22 - NumChannels
     0,0,0,0,            // 24 - SampleRate in Hz
     0,0,0,0,            // 28 - Byte Rate (SampleRate*NumChannels*(BitsPerSample/8)
     4,0,                // 32 - BlockAlign (== NumChannels * BitsPerSample/8)
     16,0,               // 34 - BitsPerSample
-    22,0,                // 36 - wav extension  
-    (22 bytes)
-    'f','a','c','t',    // 60 - fact chunk
+    22,0,               // 36 - wav extension  (0 or 22 bytes)
+    if not 0:
+    0,0,                // 38 - number of valid bits
+    0,0,0,0,            // 40 - speaker position mask
+    [16 B]              // 44 - GUID including WAV_FORMAT_PCM or WAV_FORMAT_EXTENSIBLE
+    'f','a','c','t',    // 60 - fact chunk, optional for PCM here minimum)
     0,0,0,4,            // 64 - net length of fact chunk
-    0,0,0,0,            // 68 - number of samples written out (uint32_t)
-    'd','a','t','a',    // 72 - Sunchunk2IDO
-    0,0,0,0             // 76 - 80 Subchunk2Size
+    0,0,0,0,            // 68 - number of samples written per channel out (uint32_t)
+   // some software pack up various tags in here... + x bytes
+    'd','a','t','a',    // 72 + x - Sunchunk2IDO
+    0,0,0,0             // 76 + x - 80 Subchunk2Size = filesize - padbyte - N
   };
+  Samples follow.
+  Pad byte at EF if sample count is odd.
 #endif
  
 /* wavwritehdr:  write .wav headers as follows:
@@ -129,7 +141,7 @@ Extended Wav:
 #define READ_2_bytes uint16_read_reverse(p), p+=2;
 
   /* RIFF chunk */
-/* 0-3 */ header->chunk_id    =READ_4_bytes
+/* 0-3 */   header->chunk_id    =READ_4_bytes
 /* 4-7 */   header->chunk_size  =READ_4_bytes
 /* 8-11 */  header->chunk_format=READ_4_bytes
 
@@ -143,18 +155,53 @@ Extended Wav:
 /* 32-33 */ header->byte_p_spl=READ_2_bytes
 /* 34-35 */ header->bit_p_spl =READ_2_bytes
 
+/* We diagnosed for WAV_FORMAT_EXTENSIBLE earlier */
+
 if (header->is_extensible)
 {
 /* 36-37 +22*/ header->wavext =READ_2_bytes
-            p+=header->wavext;
+
+/* skipping extension subchunk */
+
+               p += header->wavext; // 0 or 22
+
 /* 38-41 +22*/ header->fact_chunk=READ_4_bytes
-/* 42-45 +22*/ header->fact_length=READ_4_bytes
-/* 46-49 +22*/ header->n_spl=READ_4_bytes
+if (memcmp(p, "fact", 4) == 0)
+  {
+    /* 42-45 +22*/ header->fact_length=READ_4_bytes
+    /* 46-49 +22*/ header->n_spl=READ_4_bytes
+    // loop for 'data' chunk
+
+          do
+          {
+
+              if ((p = memchr(p, 'd', MAX_HEADER_SIZE-1-(p - buffer))) == NULL)
+              {
+                  printf(""ANSI_COLOR_RED"[WAR]"ANSI_COLOR_RESET"  Could not find substring 'data' among %d characters\n", MAX_HEADER_SIZE);
+                  if (globals.debugging)
+                  {
+                      hexdump_header(infile, MAX_HEADER_SIZE);
+                  }
+
+                  return BAD_HEADER;
+              }
+
+
+              if ((*(p + 1) == 'a') && (*(p + 2) == 't') && (*(p + 3) == 'a')) break;
+
+          }
+          while (p - buffer < MAX_HEADER_SIZE);
+
+          header->data_chunk=READ_4_bytes
+  }
+  else
+   header->data_chunk = header->fact_chunk;
 }
+else
 /* standard header: DATA chunk */
 /* 36-39 or 50-53 +22*/ header->data_chunk=READ_4_bytes
-/* 40-43 or 54-57 +22*/ header->data_size= uint32_read_reverse(p);
 
+/* 40-43 or 54-57 +22*/ header->data_size= uint32_read_reverse(p);
 
   /* point to beginning of file */
   rewind(infile);
