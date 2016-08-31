0a26141a96f8f489f73365dd48c64ca312f2f4aa
Fabrice Nicol <fabrice.nicol@gmail.com>
2016-08-31 15:46:15 +0200

    add VERSIONING

--- mk/man2html.global.mk
+++ mk/man2html.global.mk
@@ -1,9 +1,9 @@
 man2html_LIB=
 man2html_LINK=
-MAYBE_man2html=man2html
+MAYBE_man2html=
 HAVE_man2html=no
 HAVE_EXTERNAL_man2html=@HAVE_EXTERNAL_man2html@
-CONFIGURE_man2html_FLAGS=bindir=/mnt/fab/Dev/dvd-audio-dev/local/bin mandir=/mnt/fab/Dev/dvd-audio-dev/local/share/man 
-man2html_BUILD=yes
+CONFIGURE_man2html_FLAGS=
+man2html_BUILD=
 WITH_man2html=@WITH_man2html@
-man2html_DEPENDENCY=Makefile /mnt/fab/Dev/dvd-audio-dev/man2html
+man2html_DEPENDENCY=Makefile /home/fab/Dev/dvda-author-dev/
