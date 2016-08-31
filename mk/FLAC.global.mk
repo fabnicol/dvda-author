b1e7d3d39f990515e7911c5ce338decb8b87362d
Fabrice Nicol <fabrice.nicol@gmail.com>
2016-08-31 15:46:15 +0200

    add VERSIONING
    Former-commit-id: 0a26141a96f8f489f73365dd48c64ca312f2f4aa
    Former-commit-id: e2f4a831f20c37260c2065694da19ae0f64d9f5f

--- mk/FLAC.global.mk
+++ mk/FLAC.global.mk
@@ -1,9 +1,9 @@
-FLAC_LIB=/mnt/fab/Dev/dvd-audio-dev/local/lib/libFLAC.a
+FLAC_LIB=/home/fab/Dev/dvda-author-dev/local/lib/libFLAC.a
 FLAC_LINK=
 MAYBE_FLAC=flac-1.3.1
 HAVE_FLAC=@HAVE_FLAC@
 HAVE_EXTERNAL_FLAC=@HAVE_EXTERNAL_FLAC@
-CONFIGURE_FLAC_FLAGS=--enable-static --disable-shared --disable-fast-install --with-ogg-libraries=/mnt/fab/Dev/dvd-audio-dev/local/lib --with-ogg-includes=/mnt/fab/Dev/dvd-audio-dev/local/include/ogg        --disable-thorough-tests --disable-oggtest --disable-doxygen-docs --disable-xmms-plugin --disable-doxygen-docs --prefix=/mnt/fab/Dev/dvd-audio-dev/local CPPFLAGS=-I/mnt/fab/Dev/dvd-audio-dev/local/include 
+CONFIGURE_FLAC_FLAGS=--enable-static --disable-shared --disable-fast-install --with-ogg-libraries=/home/fab/Dev/dvda-author-dev/local/lib --with-ogg-includes=/home/fab/Dev/dvda-author-dev/local/include/ogg        --disable-thorough-tests --disable-oggtest --disable-doxygen-docs --disable-xmms-plugin --disable-doxygen-docs --prefix=/home/fab/Dev/dvda-author-dev/local CPPFLAGS=-I/home/fab/Dev/dvda-author-dev/local/include 
 flac_BUILD=@flac_BUILD@
 WITH_FLAC=@WITH_FLAC@
-FLAC_DEPENDENCY=Makefile /mnt/fab/Dev/dvd-audio-dev/flac-1.3.1
+FLAC_DEPENDENCY=Makefile /home/fab/Dev/dvda-author-dev/flac-1.3.1
