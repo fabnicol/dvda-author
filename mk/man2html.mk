642cee4a9694c211914f71a13d212e96c2a38338
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2018-02-18 20:25:38 +0100

    restart
    Former-commit-id: 784159998a7b497bb684cd39d2b0d526babafed1 [formerly 06d58cd0be8d275a5812529d680a4e51644dafbd]
    Former-commit-id: 60c628a8c0230bbfd5a5577ebfacd23b7073f82b
    Former-commit-id: 31e06b96f9e9eba83f840a0bef9325ee4200892a

--- mk/man2html.mk
+++ mk/man2html.mk
@@ -1,10 +1,10 @@
 
-include /home/fab/Dev/dvda-author-dev/mk/man2html.global.mk
+include /home/fab/Desktop/dvd-audio/mk/man2html.global.mk
 
 man2html_MAKESPEC=auto
 man2html_CONFIGSPEC=exe
 man2html_TESTBINARY=man2html
 
-/home/fab/Dev/dvda-author-dev/depconf/man2html.depconf: $(man2html_DEPENDENCY)
+/home/fab/Desktop/dvd-audio/depconf/man2html.depconf: $(man2html_DEPENDENCY)
 	$(call depconf,man2html,noconfigure,$(CONFIGURE_man2html_FLAGS) CFLAGS=)
 
