333ed8f96dbf1f0ce3b360c925ec578ca5881192
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2018-02-18 20:25:38 +0100

    restart

--- mk/FLAC.mk
+++ mk/FLAC.mk
@@ -1,9 +1,9 @@
-include /home/fab/Dev/dvda-author-dev/mk/FLAC.global.mk
+include /home/fab/Desktop/dvd-audio/mk/FLAC.global.mk
 
 FLAC_MAKESPEC=auto
 FLAC_CONFIGSPEC=lib
-FLAC_DEPENDENCY=/home/fab/Dev/dvda-author-dev/depconf/libogg.depconf 
+FLAC_DEPENDENCY=/home/fab/Desktop/dvd-audio/depconf/libogg.depconf 
 FLAC_TARGETLIB=libFLAC.a
 
-/home/fab/Dev/dvda-author-dev/depconf/FLAC.depconf: $(FLAC_DEPENDENCY)
+/home/fab/Desktop/dvd-audio/depconf/FLAC.depconf: $(FLAC_DEPENDENCY)
 	$(call depconf,FLAC,"","",CFLAGS=)
