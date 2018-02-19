b34bd6deb7db0c86fdaa2b29fda9538c0b3440d4
Fabrice Nicol <fabrnicol@gmail.com>
2018-02-19 02:14:11 +0100

    buils system fixes. TOOD: upload new lplex package and wxgtk-2.9 dep
    Former-commit-id: 4c9dec585d426f2bef193fe572f9b1b8e2562b55 [formerly 6992a850a0b55bfe39ef3cd47ebbb3a1d1efe507]
    Former-commit-id: c789e67cd9560595ac9aa9119a5efcea899a485d
    Former-commit-id: 70fd58f69da14110f6e5a97faf2ff0a44c808746

--- mk/man2html.global.mk
+++ mk/man2html.global.mk
@@ -1,9 +1,9 @@
 man2html_LIB=
 man2html_LINK=
-MAYBE_man2html=
+MAYBE_man2html=man2html
 HAVE_man2html=no
 HAVE_EXTERNAL_man2html=@HAVE_EXTERNAL_man2html@
-CONFIGURE_man2html_FLAGS=
-man2html_BUILD=
+CONFIGURE_man2html_FLAGS=bindir=/home/fab/dvd-audio/local/bin mandir=/home/fab/dvd-audio/local/share/man 
+man2html_BUILD=yes
 WITH_man2html=@WITH_man2html@
-man2html_DEPENDENCY=Makefile /home/fab/Desktop/dvd-audio/
+man2html_DEPENDENCY=Makefile /home/fab/dvd-audio/man2html
