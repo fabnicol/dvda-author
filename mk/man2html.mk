3456f1946c5582842aa7a9c97ed2a772d8ba38af
Fabrice Nicol <fabrnicol@gmail.com>
2018-02-19 02:14:11 +0100

    buils system fixes. TOOD: upload new lplex package and wxgtk-2.9 dep

--- mk/man2html.mk
+++ mk/man2html.mk
@@ -1,10 +1,10 @@
 
-include /home/fab/Desktop/dvd-audio/mk/man2html.global.mk
+include /home/fab/dvd-audio/mk/man2html.global.mk
 
 man2html_MAKESPEC=auto
 man2html_CONFIGSPEC=exe
 man2html_TESTBINARY=man2html
 
-/home/fab/Desktop/dvd-audio/depconf/man2html.depconf: $(man2html_DEPENDENCY)
+/home/fab/dvd-audio/depconf/man2html.depconf: $(man2html_DEPENDENCY)
 	$(call depconf,man2html,noconfigure,$(CONFIGURE_man2html_FLAGS) CFLAGS=)
 
