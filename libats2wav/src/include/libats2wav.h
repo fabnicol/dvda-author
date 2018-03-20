1f687d5f204d9bccd08d24165315eafecf20c709
Fabrice Nicol <fabrnicol@gmail.com>
2018-03-20 03:23:26 +0100

    fixed dvdauthor 'bug' : was a secure_open issue (removed from libcutils). Generated --topmenu --nmenus 3 OK
    Former-commit-id: c208b6c651a60d64404e86ff92c97ef3dc03e41e [formerly fb474604d539c5fa1fefa59f031d610fb40e51a2] [formerly d4ad6deadc18a176cabf11596bbb7b3ee7c9f1b4] [formerly b989fb61ec2ca8abb6a0d0ba17a491b726a74077 [formerly 75ae90b37126c1c746bd1bc0fcd6ff9812ae8698]]
    Former-commit-id: 6925d1d2355631ac2f23b67a0264eaf3681c853b [formerly 7cda2381c68908c14c56f355373ec3f9b243c011]
    Former-commit-id: cceea75c595f4b9b6656f17a4c3685bbf48b06a7
    Former-commit-id: 2041028f6637179518ffe413ffdde96a4984a366

---
+++ libats2wav/src/include/libats2wav.h
@@ -0,0 +1,39 @@
+#ifndef LIBATS2WAV_H
+#define LIBATS2WAV_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include "structures.h"
+#include "libiberty.h"
+
+typedef struct
+{
+    FILE* fpout;
+    const char* filename;
+    int samplerate;
+    uint8_t channels;
+    uint8_t bitspersample;
+    int ntracks;
+    int started;
+    uint64_t last_sector;
+    uint64_t first_sector;
+    uint64_t numsamples;
+    uint64_t numbytes;
+    uint64_t byteswritten;
+    uint64_t pts_length;
+
+} _fileinfo_t;
+
+#define BUFFER_SIZE 3*2048
+int ats2wav(const char* filename, const char* outdir, extractlist *extract, char* player);
+
+
+#endif
---
+++ libutils/src/include/libiberty.h
@@ -0,0 +1,13 @@
+#ifndef HAVE_LIBIBERTY_H
+
+#if HAVE_libiberty_BUILD
+#include "strdup.h"
+#include "getopt.h"
+#include "getopt_int.h"
+#include "strchrnul.h"
+#include "strdup.h"
+#include "strndup.h"
+#include "strnlen.h"
+#include "getsubopt.h"
+#endif
+#endif
