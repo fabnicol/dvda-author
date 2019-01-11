759d701be14123a0b00f9015dd25e75ab2d4b9a7
Fabrice Nicol <fabrnicol@gmail.com>
2019-01-12 00:17:09 +0100

    Typo fix
    Former-commit-id: 3336e96882f400d694fa32dbdafbebc7bf564b1f [formerly f979c1383c87e0b487c6d6b7811c1d0eade2efc2]
    Former-commit-id: 3d2b583b72a228920378dd6ceea4a441737a98a5
    Former-commit-id: b639f2d0a5bdd6575881dcaa7df5081f11ae6449

--- mk/help2man.global.mk
+++ mk/help2man.global.mk
@@ -3,7 +3,7 @@ help2man_LINK=
 MAYBE_help2man=help2man-1.43.3
 HAVE_help2man=yes
 HAVE_EXTERNAL_help2man=@HAVE_EXTERNAL_help2man@
-CONFIGURE_help2man_FLAGS=--prefix=/home/fab/Dev/dvda-author/local 
+CONFIGURE_help2man_FLAGS=--prefix=/home/fab/Desktop/dvda-author-dev/local 
 help2man_BUILD=yes
 WITH_help2man=@WITH_help2man@
-help2man_DEPENDENCY=Makefile /home/fab/Dev/dvda-author/help2man-1.43.3
+help2man_DEPENDENCY=Makefile /home/fab/Desktop/dvda-author-dev/help2man-1.43.3
--- mk/mjpegtools.global.mk
+++ mk/mjpegtools.global.mk
@@ -3,7 +3,7 @@ mjpegtools_LINK=
 MAYBE_mjpegtools=mjpegtools-2.1.0
 HAVE_mjpegtools=@HAVE_mjpegtools@
 HAVE_EXTERNAL_mjpegtools=@HAVE_EXTERNAL_mjpegtools@
-CONFIGURE_mjpegtools_FLAGS=--without-gtk  --without-libdv --without-dga --without-libsdl --without-libquicktime   --disable-shared --enable-static-build --enable-static  --disable-fast-install  --prefix=/home/fab/Dev/dvda-author/local  CXXFLAGS=-fPIC  CFLAGS=-fPIC 
+CONFIGURE_mjpegtools_FLAGS=--without-gtk  --without-libdv --without-dga --without-libsdl --without-libquicktime   --disable-shared --enable-static-build --enable-static  --disable-fast-install  --prefix=/home/fab/Desktop/dvda-author-dev/local  CXXFLAGS=-fPIC  CFLAGS=-fPIC 
 mjpegtools_BUILD=yes
 WITH_mjpegtools=@WITH_mjpegtools@
-mjpegtools_DEPENDENCY=Makefile /home/fab/Dev/dvda-author/mjpegtools-2.1.0
+mjpegtools_DEPENDENCY=Makefile /home/fab/Desktop/dvda-author-dev/mjpegtools-2.1.0
