44a9974bd215699460368cdca434b96d4852dbff
Fabrice Nicol <fabrnicol@gmail.com>
2019-01-12 00:17:09 +0100

    Typo fix

--- mk/sox.global.mk
+++ mk/sox.global.mk
@@ -1,9 +1,9 @@
-sox_LIB=/home/fab/Dev/dvda-author/local/lib/libsox.a
+sox_LIB=/home/fab/Desktop/dvda-author-dev/local/lib/libsox.a
 sox_LINK=
 MAYBE_sox=sox-14.4.2
 HAVE_sox=@HAVE_sox@
 HAVE_EXTERNAL_sox=no
-CONFIGURE_sox_FLAGS=--without-libltdl --without-sndfile --without-mad --with-pkgconfigdir=no --without-flac --without-ladspa --without-twolame --without-lame --without-magic --disable-fast-install --prefix=/home/fab/Dev/dvda-author/local CFLAGS=-fPIC CXXFLAGS=-fPIC CPPFLAGS=-I/home/fab/Dev/dvda-author/local/include 
+CONFIGURE_sox_FLAGS=--without-libltdl --without-sndfile --without-mad --with-pkgconfigdir=no --without-flac --without-ladspa --without-twolame --without-lame --without-magic --disable-fast-install --prefix=/home/fab/Desktop/dvda-author-dev/local CFLAGS=-fPIC CXXFLAGS=-fPIC CPPFLAGS=-I/home/fab/Desktop/dvda-author-dev/local/include 
 sox_BUILD=yes
 WITH_sox=
-sox_DEPENDENCY=Makefile /home/fab/Dev/dvda-author/sox-14.4.2
+sox_DEPENDENCY=Makefile /home/fab/Desktop/dvda-author-dev/sox-14.4.2
