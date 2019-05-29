ed83a53611b4998a06045620c41b1d928576b4e8
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2019-05-29 04:49:44 +0200

    Fixed bug: -o deactivated with --topmenu
    Former-commit-id: 7c347328b6e5a66059b0957cf3cb87fcd2175559 [formerly f2ae20a973680607facf265cd2b5b5240175b316]
    Former-commit-id: 32c00fb98b06cef863a723a40ad17c22a6061ae2
    Former-commit-id: deecb71a17610fdf672927e17319298a5aca7b41

--- mk/ImageMagick.mk
+++
@@ -1,8 +0,0 @@
-include /home/fab/Desktop/dvda-author-dev/mk/ImageMagick.global.mk
-
-ImageMagick_MAKESPEC=auto
-ImageMagick_CONFIGSPEC=exe
-ImageMagick_TESTBINARY=convert
-
-/home/fab/Desktop/dvda-author-dev/depconf/ImageMagick.depconf: $(ImageMagick_DEPENDENCY)
-	$(call depconf,ImageMagick)
--- mk/libmpeg2.global.mk
+++
@@ -1,9 +0,0 @@
-libmpeg2_LIB=
-libmpeg2_LINK=
-MAYBE_libmpeg2=libmpeg2-0.5.1
-HAVE_libmpeg2=@HAVE_libmpeg2@
-HAVE_EXTERNAL_libmpeg2=@HAVE_EXTERNAL_libmpeg2@
-CONFIGURE_libmpeg2_FLAGS=--prefix=/home/fab/Desktop/dvda-author-dev/local 
-libmpeg2_BUILD=yes
-WITH_libmpeg2=@WITH_libmpeg2@
-libmpeg2_DEPENDENCY=Makefile /home/fab/Desktop/dvda-author-dev/libmpeg2-0.5.1
