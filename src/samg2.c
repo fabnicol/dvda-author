6a0803daccb812f08a662f7a2d0e406a1774cdbf
Fabrice Nicol <fabnicol@sourceforge.net>
2019-06-25 21:47:15 +0200

    Add counter for startsector. Should be imporved as follows: delay samg2.c until VIDEO_TS is imported and all AUDIO_TS VOBS, AUDIO_SV.IFO and AUDIO_TS.IFO are reated. Alternatively postpone writing SAMG and rewrite the corresponding bytes. Or use command-> information
    Former-commit-id: 0bf59795e4a97e6fb02ce47a9f898b896da0e67d [formerly d72ad4b630cee5a2827d6e5217de123c1c5d21ce]
    Former-commit-id: ff8438690f723790813593cb65efb6dc54d82d15
    Former-commit-id: 2cace58a8b89ad58b00fb2b50b5af985850dc12c
    Former-commit-id: ed295a445e0d30cf6f334526787e563063475f21

--- src/samg2.c
+++ src/samg2.c
@@ -75,9 +75,21 @@ uint32_t create_samg(char* audiotsdir, command_t *command, sect* sectors)
 
     i=16;
 
-    /*
-       absolute_sector_offset =startsector +  sizeof(AUDIO_PP.IFO)+sizeof(AUDIO_TS.IFO)+sizeof(AUDIO_TS.BUP)+ sizeof AUDIO_SV.IFO/BUP +size of AUDIO_TS/SV.VOB+sizeof(ATS_01_1.IFO)
-    */
+    //
+    //   absolute_sector_offset =startsector +  sizeof(AUDIO_PP.IFO)+sizeof(AUDIO_TS.IFO)+sizeof(AUDIO_TS.BUP)+ sizeof AUDIO_SV.IFO/BUP +size of AUDIO_TS/SV.VOB+sizeof(ATS_01_1.IFO)
+    //
+
+    // In principle, VIDEO_TS files should be scanned too 
+    int nb_files = 0;
+    if (count_dir_files(audiotsdir, &nb_files) == 0)
+    {
+      startsector += nb_files;
+    }
+    else
+    {
+      foutput("%s%s\n", ERR "Could not count number of files in ", audiotsdir);
+    }
+
 
     absolute_sector_offset=(uint32_t) startsector + sectors->samg + 2*(sectors->amg + sectors->asvs) + sectors->topvob + sectors->stillvob +sectors->atsi[0];
     
